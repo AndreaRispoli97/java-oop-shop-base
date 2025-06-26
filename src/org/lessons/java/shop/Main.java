0d0b7395fcea41ce3289f8236fbe8829144ca0fa
AndreaRispoli97 <Andrea9715@outlook.it>
2025-06-26 16:03:28 +0200

    fix: correct Prodotto instantiation and enhance output for asus

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Creo l'oggetto1
-        // Prodotto asus = new Prodotto(0, "TUF", "Lorem Ipsum ecc....", 2099.50f, 22);
+        Prodotto asus = new Prodotto("TUF", "Lorem Ipsum ecc....", 2099.50f, 22);
 
         // Creo l'oggetto2
         Prodotto mac = new Prodotto("Mac-Book", "Lorem Ipsum ecc....", 1699.50f, 22);
@@ -21,10 +21,11 @@ public class Main {
         // asus.randomCode(0);*Errato
 
         // System.out.println("Codice del prodotto: " + asus.codice);
-        // System.out.println("Nome del prodotto: " + asus.nome);
-        // System.out.println("Descrizione del prodotto: " + asus.descrizione);
-        // System.out.println("Prezzo del Prodotto senza iva: " + asus.prezzo + "$");
-        // System.out.println("Prezzo con Iva: " + asus.prezzoIva() + "$");
+        System.out.println("Nome del prodotto: " + asus.nome);
+        System.out.println("Nome del prodotto esteso: " + asus.nomeEsteso());
+        System.out.println("Descrizione del prodotto: " + asus.descrizione);
+        System.out.println("Prezzo del Prodotto senza iva: " + asus.prezzoBase() + "$");
+        System.out.println("Prezzo con Iva: " + asus.prezzoIva() + "$");
 
     }
 }
