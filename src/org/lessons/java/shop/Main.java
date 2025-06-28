190d48c911d9132dd3bd9ef56eca01c0c7471b78
AndreaRispoli97 <Andrea9715@outlook.it>
2025-06-28 12:52:05 +0200

    refactor: update Prodotto class with new constructors and accessors; enhance Main class output

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -4,24 +4,40 @@ public class Main {
     public static void main(String[] args) {
 
         // Creo l'oggetto1
-        Prodotto asus = new Prodotto("TUF", "Lorem Ipsum ecc....", 2099.50f, 22);
+        Prodotto asus = new Prodotto("TUF", "Asus", 2099.50f, 22);
 
         // Creo l'oggetto2
-        Prodotto mac = new Prodotto("Mac-Book", "Lorem Ipsum ecc....", 1699.50f, 22);
+        Prodotto mac = new Prodotto("MacBook", "Apple", 1699.50f, 22);
 
-        // System.out.println("Codice del prodotto: " + mac.codice);
-        System.out.println("Nome del prodotto: " + mac.nome);
-        System.out.println("Nome del prodotto esteso: " + mac.nomeEsteso());
-        System.out.println("Descrizione del prodotto: " + mac.descrizione);
-        System.out.println("Prezzo del Prodotto senza iva: " + mac.prezzoBase() + "$");
-        System.out.println("Prezzo con Iva: " + mac.prezzoIva() + "$");
+        // Creo il terzo oggetto per testare il secondo costruttore
+        Prodotto hp = new Prodotto("Spectre", "HP", 1499.99f);
 
-        // System.out.println("Codice del prodotto: " + asus.codice);
-        System.out.println("Nome del prodotto: " + asus.nome);
-        System.out.println("Nome del prodotto esteso: " + asus.nomeEsteso());
-        System.out.println("Descrizione del prodotto: " + asus.descrizione);
-        System.out.println("Prezzo del Prodotto senza iva: " + asus.prezzoBase() + "$");
-        System.out.println("Prezzo con Iva: " + asus.prezzoIva() + "$");
+        System.out.println("---DETTAGLI PRODOTTO: " + hp.getNome() + "-" + hp.getMarca() + "---");
+        // System.out.println("Codice del prodotto: " + hp.getCodice);
+        System.out.println("Nome del prodotto: " + hp.getNome());
+        System.out.println("Nome del prodotto esteso: " + hp.getNomeEsteso());
+        System.out.println("Marca del prodotto: " + hp.getMarca());
+        System.out.println("Prezzo del Prodotto senza iva: " + hp.getPrezzoBase() + "$");
+        System.out.println("Prezzo con Iva: " + hp.getPrezzoIva() + "$");
+
+        System.out.println("---DETTAGLI PRODOTTO: " + mac.getNome() + "-" + mac.getMarca() + "---");
+        // System.out.println("Codice del prodotto: " + mac.getCodice);
+        System.out.println("Nome del prodotto: " + mac.getNome());
+        System.out.println("Nome del prodotto esteso: " + mac.getNomeEsteso());
+        System.out.println("Marca del prodotto: " + mac.getMarca());
+        System.out.println("Prezzo del Prodotto senza iva: " + mac.getPrezzoBase() + "$");
+        // mac.setPrezzo(1500.00f);
+        System.out.println("Prezzo con Iva: " + mac.getPrezzoIva() + "$");
+        // mac.setIva(40);
+
+        System.out.println("---DETTAGLI PRODOTTO: " + asus.getNome() + "-" + asus.getMarca() + "---");
+        // System.out.println("Codice del prodotto: " + asus.getCodice);
+        System.out.println("Nome del prodotto: " + asus.getNome());
+        System.out.println("Nome del prodotto esteso: " + asus.getNomeEsteso());
+        System.out.println("Marca del prodotto: " + asus.getMarca());
+        System.out.println("Prezzo del Prodotto senza iva: " + asus.getPrezzoBase() +
+                "$");
+        System.out.println("Prezzo con Iva: " + asus.getPrezzoIva() + "$");
 
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -5,35 +5,87 @@ import java.util.Random;
 public class Prodotto {
 
     // attributi
-    public int codice;
-    public String nome;
-    public String descrizione;
-    public float prezzo;
-    public int iva;
+    private int codice;
+    private String nome;
+    private String marca;
+    private float prezzo;
+    private int iva;
 
     // Costruttore
-    public Prodotto(String nome, String descrizione, float prezzo, int iva) {
-        Random rand = new Random();
-        this.codice = rand.nextInt(10000);
+    public Prodotto(String nome, String marca, float prezzo, int iva) {
+        this(); // Per chiamare il secondo costruttore
         this.nome = nome;
-        this.descrizione = descrizione;
+        this.marca = marca;
         this.prezzo = prezzo;
         this.iva = iva;
     }
 
-    // Metodi
-    public float prezzoBase() {
+    // Secondo Costruttore
+    public Prodotto() {
+        Random rand = new Random();
+        this.codice = rand.nextInt(10000);
+    }
+
+    // Terzo costruttore
+    public Prodotto(String nome, String marca, float prezzo) {
+        this(nome, marca, prezzo, 22);
+        // this();
+        // this.nome = nome;
+        // this.marca = marca;
+        // this.prezzo = prezzo;
+        // this.iva = 22;
+    }
+
+    // Getter per la lettura
+    public int getCodice() {
+        return this.codice;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public float getPrezzo() {
         return this.prezzo;
     }
 
-    public String nomeEsteso() {
+    public int getIva() {
+        return this.iva;
+    }
+
+    // Setter per la scrittura
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setPrezzo(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+    // Metodi
+    public float getPrezzoBase() {
+        return this.prezzo;
+    }
+
+    public String getNomeEsteso() {
         return codice + "-" + nome;
     }
 
-    public float prezzoIva() {
+    public float getPrezzoIva() {
         float risultato = prezzo + (prezzo * iva / 100);
         float cifraArr = Math.round(risultato * 100) / 100f;
         return cifraArr;
     }
-
 }
